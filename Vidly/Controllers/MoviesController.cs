2236fe4825c816cfe42f81eb29b08a4f36d16ebc
briang123 <briang.sandiego@gmail.com>
2019-01-08 10:50:08 -0800

    Add movies and genres to db and display movie details

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,28 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            IEnumerable<Movie> movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int Id)
         {
-            return new List<Movie>
+            Movie movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == Id);
+            if (movie == null)
             {
-                new Movie {Id=1, Name = "Shrek"},
-                new Movie {Id=2, Name = "Wall-e"}
-            };
+                return HttpNotFound($"A movie with an ID of {Id} was not found.");
+            }
+
+            return View(movie);
         }
+
     }
 }
