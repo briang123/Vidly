921a50866dab48d9fdc5ee3fb49d780e1d73c58e
briang123 <briang.sandiego@gmail.com>
2019-01-08 17:05:40 -0800

    and'ed instead of Or'ed the customer object in viewmodel. oops!

--- Vidly/ViewModels/NewCustomerViewModel.cs
+++ Vidly/ViewModels/NewCustomerViewModel.cs
@@ -14,7 +14,7 @@ namespace Vidly.ViewModels
         {
             get
             {
-                if (Customer != null || Customer.Id != 0)
+                if (Customer != null && Customer.Id != 0)
                 {
                     return "Edit Customer";
                 }
