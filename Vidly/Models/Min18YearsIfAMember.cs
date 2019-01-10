196b7cc00f72a2efc379939cb729cca4cfe7ad1f
briang123 <briang.sandiego@gmail.com>
2019-01-10 13:29:27 -0800

    clean up Min18YearsIfAMember model

--- Vidly/Models/Min18YearsIfAMember.cs
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -10,20 +10,16 @@ namespace Vidly.Models
             var customer = (Customer)validationContext.ObjectInstance;
             if (customer.MembershipTypeId == MembershipType.Unknown ||
                 customer.MembershipTypeId == MembershipType.PayAsYouGo)
-            {
                 return ValidationResult.Success;
-            }
-            else if (customer.Birthdate == null)
-            {
-                return new ValidationResult(("Birthdate is required"));
-            }
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required");
 
             var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
 
-            return (age >= 18)
+            return age >= 18
                 ? ValidationResult.Success
                 : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
-
         }
     }
 }
