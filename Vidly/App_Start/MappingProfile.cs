2970087ac635b6238048ec5b331ee8ecc69a1a09
briang123 <briang.sandiego@gmail.com>
2019-01-12 05:56:12 -0800

    automapper setup code and for customer

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly
+{
+    public class MappingProfile : Profile
+    {
+        //commented out block is for latest version of AutoMapper vs. v4.1
+        //public static void Run()
+        //{
+        //    Mapper.Initialize(a =>
+        //    {
+        //        a.AddProfile<MappingProfile>();
+        //    });
+        //}
+
+        public MappingProfile()
+        {
+            //Mapper.CreateMap<Customer, CustomerDto>().ReverseMap(); //instead of 2 create maps model/dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +8,13 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-
 namespace Vidly
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
